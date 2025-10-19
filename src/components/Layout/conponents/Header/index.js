05e9b466e8e984d3052dc7141b1feec19bcdf403
kiethocweb0908 <kiethocweb0908@gmail.com>
2025-10-19 22:16:58 +0700

    Xây Dựng UI Phần Header Dự Án Tiktok Clone (Phần 1)

--- src/components/Layout/conponents/Header/index.js
+++ src/components/Layout/conponents/Header/index.js
@@ -1,14 +1,31 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/image';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmarkCircle, faSpinner, faGlassMartini, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
-
 function Header() {
     return (
         <h1 className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/*  */}
-                {/*  */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+
+                <div className={cx('search')}>
+                    <input type="text" placeholder="search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faXmarkCircle} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('action')}>a</div>
             </div>
         </h1>
     );
